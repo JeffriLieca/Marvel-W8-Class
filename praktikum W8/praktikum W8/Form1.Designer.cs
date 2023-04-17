0dae3cfb96d5b29fc57d811321eaa591bb88bdf3
JeffriLieca <95264893+JeffriLieca@users.noreply.github.com>
2023-04-17 09:47:31 +0700

    Add files via upload

---
+++ praktikum W8/praktikum W8/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace praktikum_W8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.box = new System.Windows.Forms.ComboBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.teamDataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.findMatchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.data1 = new System.Windows.Forms.DataGridView();
+            this.data2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.data1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.data2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // box
+            // 
+            this.box.FormattingEnabled = true;
+            this.box.Location = new System.Drawing.Point(12, 37);
+            this.box.Name = "box";
+            this.box.Size = new System.Drawing.Size(152, 21);
+            this.box.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.teamDataToolStripMenuItem,
+            this.findMatchToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // teamDataToolStripMenuItem
+            // 
+            this.teamDataToolStripMenuItem.Name = "teamDataToolStripMenuItem";
+            this.teamDataToolStripMenuItem.Size = new System.Drawing.Size(74, 20);
+            this.teamDataToolStripMenuItem.Text = "Team Data";
+            this.teamDataToolStripMenuItem.Click += new System.EventHandler(this.teamDataToolStripMenuItem_Click);
+            // 
+            // findMatchToolStripMenuItem
+            // 
+            this.findMatchToolStripMenuItem.Name = "findMatchToolStripMenuItem";
+            this.findMatchToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.findMatchToolStripMenuItem.Text = "Find Match";
+            this.findMatchToolStripMenuItem.Click += new System.EventHandler(this.findMatchToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.data2);
+            this.panel1.Controls.Add(this.data1);
+            this.panel1.Location = new System.Drawing.Point(0, 64);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 387);
+            this.panel1.TabIndex = 2;
+            // 
+            // data1
+            // 
+            this.data1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.data1.Location = new System.Drawing.Point(32, 3);
+            this.data1.Name = "data1";
+            this.data1.Size = new System.Drawing.Size(342, 381);
+            this.data1.TabIndex = 0;
+            // 
+            // data2
+            // 
+            this.data2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.data2.Location = new System.Drawing.Point(424, 3);
+            this.data2.Name = "data2";
+            this.data2.Size = new System.Drawing.Size(342, 381);
+            this.data2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(171, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Find";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.box);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.data1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.data2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox box;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem teamDataToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem findMatchToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView data2;
+        private System.Windows.Forms.DataGridView data1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ praktikum W8/praktikum W8/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace praktikum_W8
+{
+    public partial class Form1 : Form
+    {
+        string simpen = "";
+        string nyimpen = "";
+        int m = 0;
+        string connectionstring = "server=localhost;uid=root;pwd=;database=premier_league";
+        MySqlConnection mySqlConnection;
+        MySqlCommand mySqlCommand;
+        MySqlDataAdapter SqlDataAdapter;
+        string sqlquery;
+        DataTable a1 = new DataTable();
+        DataTable a2 = new DataTable();
+        DataTable a3 = new DataTable();
+        DataTable a4 = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mySqlConnection = new MySqlConnection(connectionstring);
+            mySqlConnection.Open();
+            sqlquery = "SELECT team_name as `Team Name`, team_id as `Team ID` FROM team;";
+            mySqlCommand = new MySqlCommand(sqlquery, mySqlConnection);
+            SqlDataAdapter = new MySqlDataAdapter(mySqlCommand);
+            SqlDataAdapter.Fill(a1);
+            box.DataSource = a1;
+            box.DisplayMember = "Team Name";
+            box.ValueMember = "Team ID";
+        }
+
+        private void teamDataToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            m = 0;
+            data2.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (m == 0)
+            {
+                a2 = new DataTable();
+                simpen = box.Text.ToString();
+                sqlquery = "SELECT player_name as `Player Name`, team_name as `Team Name`, playing_pos as `Player Position`, weight as `Weight`, height as `Height` FROM player p, team t\r\nwhere p.team_id = t.team_id and team_name = " + "\"" + simpen + "\"" + ";";
+                mySqlCommand = new MySqlCommand(sqlquery, mySqlConnection);
+                SqlDataAdapter = new MySqlDataAdapter(mySqlCommand);
+                SqlDataAdapter.Fill(a2);
+                data1.DataSource = a2;
+
+                a3 = new DataTable();
+                nyimpen = box.SelectedValue.ToString();
+                sqlquery = "SELECT manager_name as `Mananger Name`, m.birthdate as `Birth Date`, m.nationality_id as `National ID` FROM manager m, player p\r\nwhere m.manager_id = p.player_id and p.team_id = " + "\"" + nyimpen + "\"" + ";";
+                mySqlCommand = new MySqlCommand(sqlquery, mySqlConnection);
+                SqlDataAdapter = new MySqlDataAdapter(mySqlCommand);
+                SqlDataAdapter.Fill(a3);
+                data2.DataSource = a3;
+            }
+            else if (m == 1)
+            {
+                a4 = new DataTable();
+                nyimpen = box.SelectedValue.ToString();
+                sqlquery = $"SELECT t1.team_name as `Home Team`, t2.team_name as `Away Team`, match_date as `Match Date`, goal_home as `Home Goal`, goal_away as `Away Goal`, r.referee_name as `Referee Name` FROM `match` m, referee r, team t1, team t2\r\nwhere m.referee_id = r.referee_id and team_home = t1.team_id and team_away = t2.team_id and t1.team_id = '{nyimpen}';";
+                mySqlCommand = new MySqlCommand(sqlquery, mySqlConnection);
+                SqlDataAdapter = new MySqlDataAdapter(mySqlCommand);
+                SqlDataAdapter.Fill(a4);
+                sqlquery = $"SELECT t1.team_name as `Home Team`, t2.team_name as `Away Team`, match_date as `Match Date`, goal_home as `Home Goal`, goal_away as `Away Goal`, r.referee_name as `Referee Name` FROM `match` m, referee r, team t1, team t2\r\nwhere m.referee_id = r.referee_id and team_home = t1.team_id and team_away = t2.team_id and t2.team_id = '{nyimpen}';";
+                mySqlCommand = new MySqlCommand(sqlquery, mySqlConnection);
+                SqlDataAdapter = new MySqlDataAdapter(mySqlCommand);
+                SqlDataAdapter.Fill(a4);
+                data1.DataSource = a4;
+            }
+        }
+
+        private void findMatchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            data2.Visible = false;
+            m = 1;
+        }
+    }
+}
